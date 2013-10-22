bf0fa72bedf419befab0e2a74caddc4baf9ec79f
Andrew Gallant <jamslam@gmail.com>
2013-10-21 22:57:32 -0400

    command line argument clean up

--- Config.hs
+++ Config.hs
@@ -4,11 +4,12 @@ module Config
   )
 where
 
-import Data.List (intercalate)
+import Data.Char (isSpace)
+import Data.List (dropWhileEnd, intercalate)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import qualified System.Console.GetOpt as O
-import System.IO (Handle, stdin, stdout, stderr)
+import System.IO (Handle, IOMode(..), stdin, stdout, stderr, openFile)
 import Text.Printf (HPrintfType, hPrintf)
 
 data Config =
@@ -17,12 +18,10 @@ data Config =
          , cquiet :: Bool
          }
 
-defaultConfig :: Handle -- ^ The input file handle.
-                 -> Handle -- ^ The output file handle.
-                 -> Config
-defaultConfig i o =
-  Config { cin = i
-         , cout = o
+defaultConfig :: Config
+defaultConfig =
+  Config { cin = stdin
+         , cout = stdout
          , cquiet = False
          }
 
@@ -30,15 +29,36 @@ configIO :: IO Config
 configIO = do
   args <- getArgs
   case O.getOpt O.Permute opts args of
+    (flags, [], []) -> foldl (\c app -> app c) (return defaultConfig) flags
     (_, _, errs@(_:_)) -> do
-      ef "Error(s) parsing flags:\n\t%s" (intercalate "\n\t" errs)
+      ef "Error(s) parsing flags:\n\t%s\n" $
+        intercalate "\n\t" $ map strip errs
       exitFailure
-    (flags, _, []) ->
-      foldl (\c app -> app c) (return $ defaultConfig stdin stdout) flags
+    (_, _, []) -> do
+      ef "erd does not have any positional arguments.\n\n"
+      usageExit
 
 opts :: [O.OptDescr (IO Config -> IO Config)]
 opts =
-  [ O.Option "q" ["quiet"]
+  [ O.Option "i" ["input"]
+      (O.ReqArg (\fpath cIO -> do
+                   c <- cIO
+                   i <- openFile fpath ReadMode
+                   return $ c { cin = i }
+                )
+                "FILE")
+      ("When set, input will be read from the given file.\n"
+       ++ "Otherwise, stdin will be used.")
+  , O.Option "o" ["output"]
+      (O.ReqArg (\fpath cIO -> do
+                    c <- cIO
+                    o <- openFile fpath WriteMode
+                    return $ c { cout = o }
+                )
+                "FILE")
+      ("When set, output will be written to the given file.\n"
+       ++ "Otherwise, stdout will be used.")
+  , O.Option "q" ["quiet"]
       (O.NoArg $ fmap (\c -> c { cquiet = True }))
       "When set, all warnings will be suppressed."
   , O.Option "h" ["help"]
@@ -50,7 +70,10 @@ usageExit :: IO a
 usageExit = usage >> exitFailure
 
 usage :: IO a
-usage = ef "%s\n" $ O.usageInfo "Usage: erd [in-file] out-file" opts
+usage = ef "%s\n" $ O.usageInfo "Usage: erd [flags]" opts
 
 ef :: HPrintfType r => String -> r
 ef = hPrintf stderr
+
+strip :: String -> String
+strip = dropWhile isSpace . dropWhileEnd isSpace
